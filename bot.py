ff7426970d254448f8850cf872317dba052f64d3
Sourcery AI <>
2022-10-03 16:22:47 +0000

    'Refactored by Sourcery'

--- bot.py
+++ bot.py
@@ -45,28 +45,29 @@ async def _(event):
 async def helpp(event):
     await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)")
 
-@datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
+@datgbot.on(events.NewMessage(incoming=True, chats=frm))
 async def _(event): 
-    if not event.is_private:
-        try:
-            if event.poll:
-                return
-            if event.photo:
-                photo = event.media.photo
-                await datgbot.send_file(tochnl, photo, caption = event.text, link_preview = False)
-            elif event.media:
-                try:
-                    if event.media.webpage:
-                        await datgbot.send_message(tochnl, event.text, link_preview = False)
-                        return
-                except:
-                    media = event.media.document
-                    await datgbot.send_file(tochnl, media, caption = event.text, link_preview = False)
+    if event.is_private:
+        return
+    try:
+        if event.poll:
+            return
+        if event.photo:
+            photo = event.media.photo
+            await datgbot.send_file(tochnl, photo, caption = event.text, link_preview = False)
+        elif event.media:
+            try:
+                if event.media.webpage:
+                    await datgbot.send_message(tochnl, event.text, link_preview = False)
                     return
-            else:
-                await datgbot.send_message(tochnl, event.text, link_preview = False)
-        except:
-            print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
+            except:
+                media = event.media.document
+                await datgbot.send_file(tochnl, media, caption = event.text, link_preview = False)
+                return
+        else:
+            await datgbot.send_message(tochnl, event.text, link_preview = False)
+    except:
+        print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
 
 print("Bot has started.")
